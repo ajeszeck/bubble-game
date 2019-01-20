2b61852321e1e16081eb12aec79189d0866bf0c2
Anna J <ajeszeck@gmail.com>
2019-01-20 11:59:28 -0800

    Add modal to display final score.

--- public/index.js
+++ public/index.js
@@ -1,12 +1,14 @@
 let viewportHeight = window.innerHeight;
 let viewportWidth = window.innerWidth;
 let speed = 50;
-let mostRecentSpeed = 1;
+let timeLeft;
+let downloadTimer;
 let scoreAndSpeedContainerHeight = document.getElementById("score-and-speed").offsetHeight;
+let modal = document.getElementById("modal");
 let score = 0;
+let started = false;
 var gameplay;
 let bubbleAnimations = [];
-
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -46,13 +48,26 @@ function onClick(event) {
   generateBubble();
 }
 
+function close(event) {
+  console.log("clicked");
+  modal.style.display = "none";
+  started = false;
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    started = false;
+  }
+}
+
 function generateBubble() {
   let bubble = document.createElement("div");
   let bubbleZone = document.getElementById("bubble-zone");
   let bubbleDimensions = randomNumber(10, 100);
   const leftPosition = horizontalPosition(viewportWidth, bubbleDimensions);
-  let startingPosition = scoreAndSpeedContainerHeight - bubbleDimensions;
-  bubble.setAttribute("style", `position:absolute;background-color:lightblue; height: ${bubbleDimensions}px; width:${bubbleDimensions}px; opacity: 0.8; box-shadow: 0px 2px 47px -14px rgba(0,0,0,0.75); left: ${leftPosition}px; top: 249px;`);
+  let startingPosition = scoreAndSpeedContainerHeight + bubbleDimensions;
+  bubble.setAttribute("style", `position:absolute; background-color:white; height: ${bubbleDimensions}px; width:${bubbleDimensions}px; opacity: 0.8; box-shadow: 0px 2px 47px -14px rgba(0,0,0,0.75); left: ${leftPosition}px; top: 249px;`);
   let newBubble = bubble.animate(
     [
       { transform: 'translateY(0)' },
@@ -64,7 +79,7 @@ function generateBubble() {
     });
     newBubble.playbackRate = speed / 100;
   bubbleAnimations.push(newBubble);
-  bubble.setAttribute("onclick", "onClick(event)");
+  bubble.setAttribute("onClick", "onClick(event)");
   bubble.setAttribute("onAnimationEnd", "onAnimationEnd(event)");
   bubble.setAttribute("points", `${Math.round(100 / bubbleDimensions)}`);
   bubble.classList.add("bubble");
@@ -77,14 +92,31 @@ function stopGame() {
   Array.from(bubbles).map(function(bubble) {
     bubble.parentNode.removeChild(bubble);
   });
+  clearInterval(downloadTimer);
+  let endStatement = "You got " + score + " points!"
+  document.getElementById("final-score").innerText = endStatement;
+  document.getElementById("modal").style.display = "block";
 }
 
 function startGame() {
-  gamePlay = setInterval(generateBubble, 1000);
-  let rangeInput = document.getElementById("speedRange");
-  rangeInput.value = speed;
-  // let currentSpeedContainer = document.getElementById("currentSpeed");
-  // currentSpeedContainer.innerText = speed;
+  if (!started) {
+    started = true;
+    gamePlay = setInterval(generateBubble, 1000);
+    let rangeInput = document.getElementById("speedRange");
+    rangeInput.value = speed;
+    timeLeft = 3;
+    downloadTimer = setInterval(() => {
+      timeLeft--;
+      document.getElementById("countdown").value = timeLeft;
+      document.getElementById("countdown-timer").innerText = timeLeft;
+      if (timeLeft <= 0) {
+        clearInterval(downloadTimer);
+        stopGame();
+      }}, 1000);
+    // let currentSpeedContainer = document.getElementById("currentSpeed");
+    // currentSpeedContainer.innerText = speed;
+  }
+
 }
 
 window.onload = function() {
